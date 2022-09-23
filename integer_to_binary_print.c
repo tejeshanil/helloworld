f273a386bcde6064cd7fdf1fdcf1c00a8bccf690
user.tejeshanil <user.nagatejesh@gmail.com>
2022-09-23 13:24:20 +0530

    Added integer_to_binary_print sample

---
+++ integer_to_binary_print.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void displayBits(int x);
+int main()
+{
+    int x;
+    printf("enter an integer");
+    scanf("%d",&x);
+    displayBits(x);
+    return 0;
+}
+
+void displayBits(int x)
+{
+    int mask, i;
+    for (i = 31 ; i>=0 ; i--)
+    {
+        mask = 1 << i;
+        putchar((x&mask)?'1':'0');
+
+        if(i%8==0)
+            putchar(' ');
+
+    }
+    printf("\n");
+
+}
